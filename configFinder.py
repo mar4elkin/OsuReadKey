8fe6428d5fcece7c30732160705a3997376c0ab6
Mark Tamarov <googlcraftlol@gmail.com>
2020-03-14 22:54:18 +0300

    fixed finder

--- configFinder.py
+++ configFinder.py
@@ -1,4 +1,6 @@
 import glob, os
+import os.path
+from os import path
 from osPaths import OsPaths
 
 osSelector = OsPaths()
@@ -11,10 +13,10 @@ class ConfigFinder(object):
     
     def findConfig(self):
         self.configs = []
+        if (path.exists(selecters[1])):
+            os.chdir(selecters[1])
+            for file in glob.glob("*.ocon"):
+                self.configs.append(file)
+            return self.configs
 
-        os.chdir(selecters[1])
-        for file in glob.glob("*.ocon"):
-            self.configs.append(file)
-
-        return self.configs
 
